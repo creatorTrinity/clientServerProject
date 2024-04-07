c60f3fa17f320e586a93947ad22aa775ed0264f1
creatorTrinity <119303312+creatorTrinity@users.noreply.github.com>
2024-04-07 13:27:54 +0530

    updated after implementing the add,update,delete
    updated after implementing the add,update,delete

--- project/client/client.c
+++ project/client/client.c
@@ -14,13 +14,33 @@ void serverConnect( int clientMsgId, int clientPID,char *serverResponse)
     char ch;
     time_t currentTime;
 
-
-
     while(1)
     {
-        /*start taking input from client and send the request to the server*/
+        printf("\n\n!!!Please choose the below options!!!\n");
+        printf("\n---------------------------------------------------\n");
+        printf("\nEnter 1.1 ->> to sort the Emp records by first name");
+        printf("\nEnter 1.2 ->> to sort the Emp records by last name");
+        printf("\nEnter 1.3 ->> to sort the Emp records by emp ID");
+        printf("\nEnter 1.4 ->> to sort the Emp records by year of experience");
+        printf("\nEnter 2.1 ->> to search the Emp records  by first name (e.g-> 2.1-RAM)");
+        printf("\nEnter 2.2 ->> to search the Emp records  by last name  (e.g-> 2.2-DEV)");
+        printf("\nEnter 2.3 ->> to search the Emp records  by emp ID     (e.g-> 2.3-4578)");
+        printf("\nEnter 3.0 ->> to search the Emp records  by skill set  (e.g-> 3.0-JAVA)");
+        printf("\nEnter 4.0 ->> to search the Emp records  by exp        (e.g-> 4.0-2.5)");
 
-        printf("Please enter the query for server\n");
+        printf("\n\n--------Options for Add Update Delete -------------------------\n\n");
+        printf("\nTo add Emp record <add-firstName lastName contactNo experience projectAllocated skillSet>");
+        printf("\nExample input <add-KOUSHIK HALDER 9836900070 18 no c,c++,python,java,.net>");
+
+        printf("\nTo add Emp record <upd-firstName lastName contactNo experience projectAllocated skillSet>");
+        printf("\nExample input <upd-KOUSHIK HALDER 9836900070 18 no c,c++,python,java,.net>");
+
+        printf("\nTo add Emp record <del-empid>");
+        printf("\nExample input <del-4578>");
+
+        printf("\n\n--------Options for Add Update Delete -------------------------\n\n");
+
+        printf("Please enter the option for server\n");
         scanf("%s",clientQueryMsg);
 
         strcpy(QueryString.query,clientQueryMsg);
